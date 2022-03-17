d7de1f42af60ba32aacc11a15d78544cd2818cba
sk33z3r <sk33z3r@protonmail.com>
2022-03-16 23:06:15 -0400

    add: docker-compose down

--- docker.sh
+++ docker.sh
@@ -6,5 +6,6 @@ case $1 in
     build) docker-compose build;;
     reload) docker-compose up -d --build --force-recreate;;
     up) docker-compose up -d;;
+    down) docker-compose down;;
     *) docker-compose exec php $@;;
 esac
