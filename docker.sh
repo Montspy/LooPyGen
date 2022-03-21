a449f7b6fb9ed16325e08d9e90c6c356c964b0cb
sk33z3r <sk33z3r@protonmail.com>
2022-03-21 16:07:30 -0400

    fix: (#18) grab current user's UID:GID and setup containers with it

--- docker.sh
+++ docker.sh
@@ -2,6 +2,22 @@
 
 name="loopygen"
 
+checkDotenv() {
+    if [ ! -f .env ]; then
+        cp .env.example .env
+    fi
+}
+
+getIDs() {
+    uid=$(id -u)
+    gid=$(id -g)
+    sed -i "s/^UID=.*$/UID=$uid/g" .env
+    sed -i "s/^GID=.*$/GID=$gid/g" .env
+}
+
+checkDotenv
+getIDs
+
 case $1 in
     build) docker-compose build;;
     reload) docker-compose up -d --build --force-recreate;;
