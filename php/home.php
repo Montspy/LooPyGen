42b15b28846c24570c2775070cf2b39ef42e6132
sk33z3r <sk33z3r@protonmail.com>
2022-04-04 03:36:40 -0400

    chore[php]: tweak homepage wording

--- php/home.php
+++ php/home.php
@@ -1,5 +1,5 @@
 <h3>Main Menu</h3>
 <div class="section">
     <a href="/setup/1"><button class="btn">IMAGE GENERATOR SETUP</button></a>
-    <a href="/config/1"><button class="btn">MINTING SETUP</button></a>
+    <a href="/config/1"><button class="btn">MINTING CONFIG SETUP</button></a>
 </div>
