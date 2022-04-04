e8fe6ea2a9a69dd87ab2cb2a852d4aa1f5f5a61b
sk33z3r <sk33z3r@protonmail.com>
2022-04-03 23:13:33 -0400

    chore[php]: (#34) add config.json generation, remove .env requirement

--- docker.sh
+++ docker.sh
@@ -3,29 +3,28 @@
 name="loopygen"
 
 checkDotenv() {
-    if [ ! -f .env ]; then
-        cp .env.example .env
-    fi
-}
-
-getIDs() {
     uid=$(id -u)
     gid=$(id -g)
-    cat .env | sed "s/^UID=.*$/UID=$uid/g" > .temp
-    cat .temp | sed "s/^GID=.*$/GID=$gid/g" > .env
-    rm .temp
+    if [ ! -f .env ]; then
+        touch .env
+        echo "# AUTOMATICALLY SET, DO NOT EDIT" >> .env
+        echo "UID=$uid" >> .env
+        echo "GID=$gid" >> .env
+    else
+        cat .env | sed "s/^UID=.*$/UID=$uid/g" > .temp
+        cat .temp | sed "s/^GID=.*$/GID=$gid/g" > .env
+        rm .temp
+    fi
 }
 
 case $1 in
     build) docker-compose build;;
     reload)
         checkDotenv
-        getIDs
         docker-compose up -d --build --force-recreate
     ;;
     up)
         checkDotenv
-        getIDs
         docker-compose up -d
     ;;
     down) docker-compose down;;
