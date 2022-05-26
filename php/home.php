17be98adccac3da76e289d71831ea0c56c51b116
sk33z3r <sk33z3r@protonmail.com>
2022-05-25 21:24:45 -0400

    feat[php] add nuke button

--- php/home.php
+++ php/home.php
@@ -1,5 +1,30 @@
-<h3>Main Menu</h3>
 <div class="section">
-    <a href="/setup/1"><button class="btn">IMAGE GENERATOR SETUP</button></a>
-    <a href="/config/1"><button class="btn">MINTING CONFIG SETUP</button></a>
+    <h3>Configuration Setup</h3>
+    <a href="/setup/1"><button class="btn">CREATE NEW COLLECTION</button></a>
+    <a href="/edit/1"><button class="btn">EDIT EXISTING COLLECTION</button></a>
+    <a href="/config/1"><button class="btn">MINT & TRANSFER CONFIG SETUP</button></a>
 </div>
+
+<div class="section">
+    <h3>Collection Actions</h3>
+    <a href="/generate/images"><button class="btn">GENERATE IMAGES</button></a>
+    <a href="/generate/metadata"><button class="btn">GENERATE METADATA</button></a>
+    <a href="/mint/1?type=collection"><button class="btn">MINT COLLECTION</button></a>
+</div>
+
+<div class="section">
+    <h3>Other Actions</h3>
+    <a href="/prepare"><button class="btn">PREPARE METADATA</button></a>
+    <a href="/mint/1?type=single"><button class="btn">MINT SINGLE ITEM</button></a>
+    <!-- <a href="/transfer/1"><button class="btn">TRANSFER</button></a> -->
+    <a href="/home?nuke=engage"><button class="btn">NUKE SENSITIVE INFO</button></a>
+</div>
+
+<?php
+
+    if (!empty($_GET['nuke'])) {
+        unlink("./config.json");
+        echo "<h3><i>Sensitive Info Deleted</i></h3>";
+    }
+
+?>
