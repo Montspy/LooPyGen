6c4cd7202bc5cc26bbb23c5f7954a8b0d45913c5
Monty <montspy@gmail.com>
2022-05-31 17:09:52 -0700

    feature[php]: add default royalty_percentage to mint config.json

--- php/config/finish.php
+++ php/config/finish.php
@@ -13,11 +13,12 @@
             <div class="section">
                 <p><b>Minter (Address, ENS, or Account ID)</b>: <?php echo $config['minter'] ?></p>
                 <p><b>Private Key</b>: <?php echo str_repeat("*", strlen($config['private_key'])) ?></p>
+                <p><b>Royalty Percentage</b>: <?php echo $config['royalty_percentage'] ?></p>
                 <p><b>NFT Type</b>: <?php echo $config['nft_type'] ?></p>
                 <p><b>Fee Token</b>: <?php echo $config['fee_token'] ?></p>
             </div>
         </div>
-        <a href="/"><button class="btn">HOME</button></a>
+        <a href="/"><button class="btn">Back to Home</button></a>
     <?php } else {
         Redirect('/config/1', false);
     }
