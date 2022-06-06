5142f05ced9cb9693fa7cbe58a2754a1ba2b637f
Monty <montspy@gmail.com>
2022-06-05 21:38:21 -0700

    feature[php]: add material icons to home

--- php/home.php
+++ php/home.php
@@ -1,25 +1,25 @@
 <section class="nav">
     <h3>Configuration Setup</h3>
-    <a href="/setup/1">CREATE NEW COLLECTION</a>
+    <a href="/setup/1"><span class="material-icons">create_new_folder</span> CREATE NEW COLLECTION</a>
     <!-- <a href="/edit/view">EDIT EXISTING COLLECTION</a> -->
-    <a href="/config/1">MINTING CONFIG SETUP</a>
-    <a href="/transfer/1">TRANSFER CONFIG SETUP</a>
+    <a href="/config/1"><span class="material-icons">settings_suggest</span> MINTING CONFIG SETUP</a>
+    <a href="/transfer/1"><span class="material-icons">settings</span> TRANSFER CONFIG SETUP</a>
 </section>
 
 <section class="nav">
     <h3>Collection Actions</h3>
-    <a href="/collection/images">GENERATE IMAGES</a>
-    <a href="/collection/metadata">GENERATE METADATA</a>
-    <a href="/collection/mint">MINT COLLECTION</a>
-    <a href="/collection/transfer">TRANSFER COLLECTION</a>
+    <a href="/collection/images"><span class="material-icons">photo_library</span> GENERATE IMAGES</a>
+    <a href="/collection/metadata"><span class="material-icons">data_object</span> GENERATE METADATA</a>
+    <a href="/collection/mint"><span class="material-icons">publish</span> MINT COLLECTION</a>
+    <a href="/collection/transfer"><span class="material-icons">keyboard_double_arrow_right</span> TRANSFER COLLECTION</a>
 </section>
 
 <section class="nav">
     <h3>Other Actions</h3>
     <!-- <a href="/prepare">PREPARE METADATA</a> -->
-    <a href="/mint">MINT SINGLE ITEM</a>
-    <a href="/transfer">TRANSFER</a>
-    <a href="/home?nuke=engage">NUKE SENSITIVE INFO</a>
+    <a href="/mint"><span class="material-icons">publish</span> MINT SINGLE ITEM</a>
+    <a href="/transfer"><span class="material-icons">keyboard_double_arrow_right</span> TRANSFER</a>
+    <a href="/home?nuke=engage" class="danger"><span class="material-icons">dangerous</span> NUKE SENSITIVE INFO</a>
     <!-- <a href="/home?update=true">UPDATE LOOPYGEN</a> -->
 </section>
 
