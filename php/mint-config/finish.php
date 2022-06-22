6761d97bcac9202590485aff71379eae91091267
sk33z3r <sk33z3r@protonmail.com>
2022-06-22 17:27:12 -0400

    feat[php]: (#72) confirm passphrases

--- php/mint-config/finish.php
+++ php/mint-config/finish.php
@@ -1,9 +1,5 @@
 <?php
 
-    if (!file_exists($mint_config) or !isset($_GET['result'])) {
-        BrowserRedirect('/mint-config/1');
-    }
-
     if ($_GET['result'] === "0") { ?>
         <h3 class="success">Configuration Successfully Encrypted</h3>
         <div id="guide">
@@ -15,11 +11,14 @@
         <div class="nav"><a href="/">BACK TO HOME</a></div>
         <div class="nav"><a href="/mint-config/1">REMAKE CONFIG</a></div>
     <?php } else {
-        unlink($mint_config); ?>
+        if (file_exists($mint_config)) { unlink($mint_config); } ?>
         <h3 class="error">Configuration Not Encrypted</h3>
+        <?php if ($_GET['result'] === "100") { ?>
+            <h3 class="warning">Your passphrases did not match.</h3>
+        <?php } ?>
         <div id="guide">
             <section>
-                <p>Your configuration was not encrypted and has been removed from the system.</p>
+                <p>Your configuration was not stored.</p>
                 <p>Please try again.</p>
             </section>
         </div>
